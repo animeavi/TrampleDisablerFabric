dbfcf1a04b9725d476911812715e898ef3acc319
Zero <zero@cock.li>
2021-01-24 20:53:00 -0500

    Add files

---
+++ src/main/java/zero/trampledisablerfabric/TrampleDisabler.java
@@ -0,0 +1,10 @@
+package zero.trampledisablerfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TrampleDisabler implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("TrampleDisabler enabled!");
+	}
+}
---
+++ src/main/java/zero/trampledisablerfabric/mixin/MixinFarmlandBlock.java
@@ -0,0 +1,25 @@
+package zero.trampledisablerfabric.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public class MixinFarmlandBlock extends Block {
+   public MixinFarmlandBlock(Settings settings) {
+      super(settings);
+   }
+
+   @Inject(method = {"onLandedUpon(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/Entity;F)V"}, at = {@At("HEAD")}, cancellable = true)
+   private void onLandedUpon(World world, BlockPos pos, Entity entity, float distance, CallbackInfo info) {
+      super.onLandedUpon(world, pos, entity, distance); // Don't cancel fall damage
+      info.cancel();
+	}
+}
